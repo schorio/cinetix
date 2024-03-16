79d71a22032b331e155a86e1cc56237158471bf4
schorio <schorioignace@gmail.com>
2024-03-16 19:30:13 +0100

    affichage d'en tete de page home

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -5,6 +5,42 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+        body: SingleChildScrollView(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top,
+              bottom: MediaQuery.of(context).padding.bottom,
+            ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Row(
+                      children: const [
+                        Text(
+                          "Hi, Schorio",
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        ),
+                        Spacer(),
+                        CircleAvatar(
+                          backgroundImage: AssetImage('assets/profile.jpg'),
+                          radius: 25,
+                          child: Align(
+                            alignment: Alignment.topRight,
+                            child: CircleAvatar(
+                              backgroundColor: Color(0xFF7c44c2),
+                              radius: 7.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ])));
   }
 }
