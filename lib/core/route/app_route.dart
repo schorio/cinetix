68d965c0761b513b4e768a5773715c448968feac
schorio <schorioignace@gmail.com>
2024-03-16 18:56:47 +0100

    creation du route pour la page d'accueil

---
+++ lib/core/route/app_route.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:cinetix/core/route/app_route_name.dart';
+import 'package:cinetix/feature/home/home_screen.dart';
+
+class AppRoute {
+  static Route<dynamic>? generate(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRouteName.home:
+        return MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
+          settings: settings,
+        );
+    }
+    return null;
+  }
+}
