0ef7274eb25f89976b267f5bf5b7fac2ed2fedc0
schorio <schorioignace@gmail.com>
2024-03-28 14:01:18 +0100

    creation du widget slider des films en salle

---
+++ lib/feature/home/widget/ensalle_widget.dart
@@ -0,0 +1,97 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:cinetix/core/model/film_model.dart';
+
+class EnSalleWidget extends StatefulWidget {
+  const EnSalleWidget({super.key});
+
+  @override
+  State<EnSalleWidget> createState() => _EnSalleWidgetState();
+}
+
+class _EnSalleWidgetState extends State<EnSalleWidget> {
+  int centerIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 400,
+      child: CarouselSlider.builder(
+        itemCount: enProjection.length,
+        options: CarouselOptions(
+          height: 400,
+          enlargeCenterPage: true,
+          enlargeStrategy: CenterPageEnlargeStrategy.height,
+          initialPage: centerIndex,
+          viewportFraction: 0.7,
+          onPageChanged: (index, reason) {
+            setState(() {
+              centerIndex = index;
+            });
+          },
+        ),
+        itemBuilder: (context, index, _) {
+          return _EnProjectionItem(
+            film: enProjection[index],
+            isCenter: index == centerIndex,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _EnProjectionItem extends StatefulWidget {
+  const _EnProjectionItem({
+    required this.film,
+    this.isCenter = false,
+  });
+
+  final Film film;
+  final bool isCenter;
+
+  @override
+  State<_EnProjectionItem> createState() => __EnProjectionItemState();
+}
+
+class __EnProjectionItemState extends State<_EnProjectionItem> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 15,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: DecorationImage(
+                  image: AssetImage(
+                    widget.film.assetImage,
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            widget.film.title,
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium
+                ?.copyWith(color: Colors.black, fontSize: 16),
+            maxLines: 1,
+          ),
+        ],
+      ),
+    );
+  }
+}
