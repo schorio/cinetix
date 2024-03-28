a36fd09c5215e62b088ff2a147a2bfd0f6969da0
schorio <schorioignace@gmail.com>
2024-03-28 09:55:25 +0100

    ajout du barre de recherche

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -17,24 +17,50 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   const SizedBox(height: 24),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: Row(
-                      children: const [
-                        Text(
-                          "Hi, Schorio",
-                          style: TextStyle(
-                            fontSize: 30,
-                          ),
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Text(
+                              "Hi, Schorio",
+                              style: TextStyle(
+                                fontSize: 30,
+                              ),
+                            ),
+                            Spacer(),
+                            CircleAvatar(
+                              backgroundImage: AssetImage('assets/profile.jpg'),
+                              radius: 25,
+                              child: Align(
+                                alignment: Alignment.topRight,
+                                child: CircleAvatar(
+                                  backgroundColor: Color(0xFF7c44c2),
+                                  radius: 7.0,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        Spacer(),
-                        CircleAvatar(
-                          backgroundImage: AssetImage('assets/profile.jpg'),
-                          radius: 25,
-                          child: Align(
-                            alignment: Alignment.topRight,
-                            child: CircleAvatar(
-                              backgroundColor: Color(0xFF7c44c2),
-                              radius: 7.0,
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 60,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey.shade300,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: 'Recherche',
+                              hintStyle: const TextStyle(color: Colors.black),
+                              prefixIcon: const Icon(
+                                Icons.search,
+                                color: Colors.black,
+                                size: 24,
+                              ),
                             ),
                           ),
                         ),
