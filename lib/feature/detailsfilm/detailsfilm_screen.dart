719220f7834c5061f393ed59ff5deed877112c6b
schorio <schorioignace@gmail.com>
2024-04-13 18:15:51 +0200

    detailsFilm : ajout du couleur gradient

--- lib/feature/detailsfilm/detailsfilm_screen.dart
+++ lib/feature/detailsfilm/detailsfilm_screen.dart
@@ -17,11 +17,26 @@ class _DetailsFilmState extends State<DetailsFilm> {
           child: Stack(
         children: [
           Container(
-            height: 600,
+            height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage(film.assetImage), fit: BoxFit.fill)),
+                    image: AssetImage(film.assetImage),
+                    fit: BoxFit.contain,
+                    alignment: const AlignmentDirectional(0, -1))),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.white.withOpacity(1),
+                      Colors.white.withOpacity(1),
+                      Colors.white.withOpacity(0.5),
+                      Colors.white.withOpacity(0),
+                    ]),
+              ),
+            ),
           )
         ],
       )),
