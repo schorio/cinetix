34539fd3ebe551ad1be29d8e4b471f00ed800697
schorio <schorioignace@gmail.com>
2024-04-16 14:22:24 +0200

    film_model : modif du type de rating

--- lib/core/model/film_model.dart
+++ lib/core/model/film_model.dart
@@ -3,7 +3,7 @@ class Film {
   final String assetImage;
   final String type;
   final String duration;
-  final String rating;
+  final int rating;
   final String synopsis;
   final bool isPlaying;
 
@@ -24,7 +24,7 @@ List<Film> prochainement = [
     assetImage: "assets/film/shang-chi.jpeg",
     type: "Action",
     duration: "1h 27m",
-    rating: "7.7/10",
+    rating: 3,
     synopsis:
         "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
     isPlaying: true,
@@ -34,7 +34,7 @@ List<Film> prochainement = [
     assetImage: "assets/film/dune.jpg",
     type: "Action",
     duration: "1h 59m",
-    rating: "7.0/10",
+    rating: 3,
     synopsis:
         "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
     isPlaying: true,
@@ -44,7 +44,7 @@ List<Film> prochainement = [
     assetImage: "assets/film/aladdin_2.jpg",
     type: "Action",
     duration: "2h 29m",
-    rating: "8.0/10",
+    rating: 3,
     synopsis:
         "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
     isPlaying: true,
@@ -54,7 +54,7 @@ List<Film> prochainement = [
     assetImage: "assets/film/greta.jpg",
     type: "Action",
     duration: "2h 29m",
-    rating: "8.0/10",
+    rating: 3,
     synopsis:
         "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
     isPlaying: true,
@@ -67,7 +67,7 @@ List<Film> enProjection = [
     assetImage: "assets/film/minion.jpeg",
     type: "Action",
     duration: "1h 27m",
-    rating: "7.7/10",
+    rating: 3,
     synopsis:
         "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
     isPlaying: true,
@@ -77,7 +77,7 @@ List<Film> enProjection = [
     assetImage: "assets/film/thor.jpeg",
     type: "Action",
     duration: "1h 59m",
-    rating: "7.0/10",
+    rating: 3,
     synopsis:
         "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
     isPlaying: true,
@@ -87,7 +87,7 @@ List<Film> enProjection = [
     assetImage: "assets/film/jurassic.jpeg",
     type: "Action",
     duration: "2h 29m",
-    rating: "8.0/10",
+    rating: 3,
     synopsis:
         "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
     isPlaying: true,
@@ -97,7 +97,7 @@ List<Film> enProjection = [
     assetImage: "assets/film/lives.jpeg",
     type: "Action",
     duration: "2h 29m",
-    rating: "8.0/10",
+    rating: 3,
     synopsis:
         "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
     isPlaying: true,
@@ -110,7 +110,7 @@ List<Film> avantPremiere = [
     assetImage: "assets/film/aladdin.jpg",
     type: "Action",
     duration: "1h 27m",
-    rating: "7.7/10",
+    rating: 3,
     synopsis:
         "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
     isPlaying: true,
@@ -120,7 +120,7 @@ List<Film> avantPremiere = [
     assetImage: "assets/film/detective.jpeg",
     type: "Action",
     duration: "1h 59m",
-    rating: "7.0/10",
+    rating: 3,
     synopsis:
         "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
     isPlaying: true,
@@ -130,7 +130,7 @@ List<Film> avantPremiere = [
     assetImage: "assets/film/narcos.jpeg",
     type: "Action",
     duration: "2h 29m",
-    rating: "8.0/10",
+    rating: 3,
     synopsis:
         "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
     isPlaying: true,
@@ -140,7 +140,7 @@ List<Film> avantPremiere = [
     assetImage: "assets/film/super-pets.jpeg",
     type: "Action",
     duration: "2h 29m",
-    rating: "8.0/10",
+    rating: 3,
     synopsis:
         "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
     isPlaying: true,
