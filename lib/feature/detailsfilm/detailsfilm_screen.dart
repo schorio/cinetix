355e4b59606b9664b6e0d3f58daaada2d4574cae
schorio <schorioignace@gmail.com>
2024-04-16 13:49:01 +0200

    detailsFilm : creatino d'un draggableScrollable

--- lib/feature/detailsfilm/detailsfilm_screen.dart
+++ lib/feature/detailsfilm/detailsfilm_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cinetix/core/design/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:cinetix/core/model/film_model.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
+import 'package:animate_do/animate_do.dart';
 
 class DetailsFilm extends StatefulWidget {
   const DetailsFilm({super.key});
@@ -13,33 +16,40 @@ class _DetailsFilmState extends State<DetailsFilm> {
   Widget build(BuildContext context) {
     final film = ModalRoute.of(context)?.settings.arguments as Film;
     return Scaffold(
-      body: SingleChildScrollView(
-          child: Stack(
+      backgroundColor: MesCouleurs.blanc,
+      body: Stack(
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(film.assetImage),
-                    fit: BoxFit.contain,
-                    alignment: const AlignmentDirectional(0, -1))),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                    colors: [
-                      Colors.white.withOpacity(1),
-                      Colors.white.withOpacity(1),
-                      Colors.white.withOpacity(0.5),
-                      Colors.white.withOpacity(0),
-                    ]),
-              ),
-            ),
-          )
+          const BlurHash(
+            hash: "LaQvwRay_3WB~qt7M{ofD%RjRjay",
+            imageFit: BoxFit.cover,
+          ),
+          DraggableScrollableSheet(
+            initialChildSize: 0.4,
+            minChildSize: 0.4,
+            maxChildSize: 0.97,
+            expand: true,
+            snap: true,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return DecoratedBox(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: MesCouleurs.primaire,
+                      blurRadius: 3,
+                      spreadRadius: 3,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+              );
+            },
+          ),
         ],
-      )),
+      ),
     );
   }
 }
