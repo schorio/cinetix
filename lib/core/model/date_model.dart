0d803cf89868022865ca14c2f377409e26c19973
schorio <schorioignace@gmail.com>
2024-05-12 15:42:22 +0300

    film_model : creation du model pour les dates

---
+++ lib/core/model/date_model.dart
@@ -0,0 +1,46 @@
+class Date {
+  final int j;
+  final String J;
+  final String M;
+  final int A;
+
+  Date({
+    required this.j,
+    required this.J,
+    required this.M,
+    required this.A,
+  });
+}
+
+List<Date> dateReservation = [
+  Date(
+    j: 5,
+    J: 'Lun',
+    M: 'Avril',
+    A: 2024,
+  ),
+  Date(
+    j: 6,
+    J: 'Mar',
+    M: 'Avril',
+    A: 2024,
+  ),
+  Date(
+    j: 7,
+    J: 'Mer',
+    M: 'Avril',
+    A: 2024,
+  ),
+  Date(
+    j: 8,
+    J: 'Jeu',
+    M: 'Avril',
+    A: 2024,
+  ),
+  Date(
+    j: 9,
+    J: 'Ven',
+    M: 'Avril',
+    A: 2024,
+  ),
+];
--- lib/core/model/film_model.dart
+++ lib/core/model/film_model.dart
@@ -1,4 +1,5 @@
 import 'package:cinetix/core/model/coms_model.dart';
+import 'package:cinetix/core/model/date_model.dart';
 
 class Film {
   final String title;
@@ -11,6 +12,7 @@ class Film {
   final String trailer;
   final List<String> actor;
   final List<ComsModel> commentaires;
+  final List<Date> date;
 
   Film({
     required this.title,
@@ -23,242 +25,243 @@ class Film {
     required this.trailer,
     required this.actor,
     required this.commentaires,
+    required this.date,
   });
 }
 
 List<Film> prochainement = [
   Film(
-    title: "Shang-Chi",
-    assetImage: "assets/film/shang-chi.jpeg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "1h 27m",
-    rating: 3,
-    synopsis:
-        "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Shang-Chi",
+      assetImage: "assets/film/shang-chi.jpeg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "1h 27m",
+      rating: 3,
+      synopsis:
+          "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "Dune",
-    assetImage: "assets/film/dune.jpg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "1h 59m",
-    rating: 3,
-    synopsis:
-        "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Dune",
+      assetImage: "assets/film/dune.jpg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "1h 59m",
+      rating: 3,
+      synopsis:
+          "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "Aladin 2",
-    assetImage: "assets/film/aladdin_2.jpg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "2h 29m",
-    rating: 3,
-    synopsis:
-        "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Aladin 2",
+      assetImage: "assets/film/aladdin_2.jpg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "2h 29m",
+      rating: 3,
+      synopsis:
+          "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "Greta",
-    assetImage: "assets/film/greta.jpg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "2h 29m",
-    rating: 3,
-    synopsis:
-        "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Greta",
+      assetImage: "assets/film/greta.jpg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "2h 29m",
+      rating: 3,
+      synopsis:
+          "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
 ];
 
 List<Film> enProjection = [
   Film(
-    title: "Minions: The Rise of Gru",
-    assetImage: "assets/film/minion.jpeg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "1h 27m",
-    rating: 3,
-    synopsis:
-        "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Minions: The Rise of Gru",
+      assetImage: "assets/film/minion.jpeg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "1h 27m",
+      rating: 3,
+      synopsis:
+          "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "Thor: Love and Thunder",
-    assetImage: "assets/film/thor.jpeg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "1h 59m",
-    rating: 3,
-    synopsis:
-        "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Thor: Love and Thunder",
+      assetImage: "assets/film/thor.jpeg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "1h 59m",
+      rating: 3,
+      synopsis:
+          "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "Jurassic World Dominion",
-    assetImage: "assets/film/jurassic.jpeg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "2h 29m",
-    rating: 3,
-    synopsis:
-        "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Jurassic World Dominion",
+      assetImage: "assets/film/jurassic.jpeg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "2h 29m",
+      rating: 3,
+      synopsis:
+          "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "Lives",
-    assetImage: "assets/film/lives.jpeg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "2h 29m",
-    rating: 3,
-    synopsis:
-        "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Lives",
+      assetImage: "assets/film/lives.jpeg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "2h 29m",
+      rating: 3,
+      synopsis:
+          "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
 ];
 
 List<Film> avantPremiere = [
   Film(
-    title: "Aladdin",
-    assetImage: "assets/film/aladdin.jpg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "1h 27m",
-    rating: 3,
-    synopsis:
-        "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Aladdin",
+      assetImage: "assets/film/aladdin.jpg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "1h 27m",
+      rating: 3,
+      synopsis:
+          "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "Detective",
-    assetImage: "assets/film/detective.jpeg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "1h 59m",
-    rating: 3,
-    synopsis:
-        "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Detective",
+      assetImage: "assets/film/detective.jpeg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "1h 59m",
+      rating: 3,
+      synopsis:
+          "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "Narcos",
-    assetImage: "assets/film/narcos.jpeg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "2h 29m",
-    rating: 3,
-    synopsis:
-        "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "Narcos",
+      assetImage: "assets/film/narcos.jpeg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "2h 29m",
+      rating: 3,
+      synopsis:
+          "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
   Film(
-    title: "SUper Pets",
-    assetImage: "assets/film/super-pets.jpeg",
-    genre: ["Action", "Drame", "Comedie"],
-    actor: [
-      "Jason Statham",
-      "Zendaya",
-      "Tom Holland",
-      "Jason Momoa",
-      "John Cena"
-    ],
-    duration: "2h 29m",
-    rating: 3,
-    synopsis:
-        "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
-    isPlaying: true,
-    trailer: "assets/trailer/trailer.mp4",
-    commentaires: coms,
-  ),
+      title: "SUper Pets",
+      assetImage: "assets/film/super-pets.jpeg",
+      genre: ["Action", "Drame", "Comedie"],
+      actor: [
+        "Jason Statham",
+        "Zendaya",
+        "Tom Holland",
+        "Jason Momoa",
+        "John Cena"
+      ],
+      duration: "2h 29m",
+      rating: 3,
+      synopsis:
+          "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
+      isPlaying: true,
+      trailer: "assets/trailer/trailer.mp4",
+      commentaires: coms,
+      date: dateReservation),
 ];
