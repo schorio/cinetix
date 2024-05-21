21c987cf50d6e446f0ef2bf74459f8f7e0f849a4
schorio <schorioignace@gmail.com>
2024-05-21 17:24:47 +0300

    DateModel : ajout d'attribut mois en chiffre

--- lib/core/model/date_model.dart
+++ lib/core/model/date_model.dart
@@ -1,12 +1,14 @@
 class Date {
   final int j;
   final String J;
+  final int m;
   final String M;
   final int A;
 
   Date({
     required this.j,
     required this.J,
+    required this.m,
     required this.M,
     required this.A,
   });
@@ -16,30 +18,56 @@ List<Date> dateReservation = [
   Date(
     j: 5,
     J: 'Lun',
+    m: 4,
     M: 'Avril',
     A: 2024,
   ),
   Date(
     j: 6,
     J: 'Mar',
+    m: 4,
     M: 'Avril',
     A: 2024,
   ),
   Date(
     j: 7,
     J: 'Mer',
+    m: 4,
     M: 'Avril',
     A: 2024,
   ),
   Date(
     j: 8,
     J: 'Jeu',
+    m: 4,
     M: 'Avril',
     A: 2024,
   ),
   Date(
     j: 9,
     J: 'Ven',
+    m: 4,
+    M: 'Avril',
+    A: 2024,
+  ),
+  Date(
+    j: 10,
+    J: 'Sam',
+    m: 4,
+    M: 'Avril',
+    A: 2024,
+  ),
+  Date(
+    j: 11,
+    J: 'Dim',
+    m: 4,
+    M: 'Avril',
+    A: 2024,
+  ),
+  Date(
+    j: 12,
+    J: 'Lun',
+    m: 4,
     M: 'Avril',
     A: 2024,
   ),
