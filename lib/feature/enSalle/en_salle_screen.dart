257d574a32aff86b4ffb9ce90be92ed65a8c5d63
schorio <schorioignace@gmail.com>
2024-05-27 17:30:02 +0300

    EnSalle : creation du page pour voir les films en salle

---
+++ lib/feature/enSalle/en_salle_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EnSalle extends StatefulWidget {
+  const EnSalle({super.key});
+
+  @override
+  State<EnSalle> createState() => _EnSalleState();
+}
+
+class _EnSalleState extends State<EnSalle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueAccent,
+    );
+  }
+}
