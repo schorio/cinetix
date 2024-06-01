e40a2b05c5a144499420924ab411b83114bd248e
schorio <schorioignace@gmail.com>
2024-06-01 07:53:26 +0300

    Home : assigner la page ProchainementScreen à un boutton

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -145,6 +145,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                         child: TitleWidget(
                           title: "Prochainement",
                           color: couleurDominant,
+                          route: AppRouteName.prochainement,
                         ),
                       ),
                       Padding(
