e9419fb410ba982ab6b0b2a6a027dbb8922d4c04
schorio <schorioignace@gmail.com>
2024-06-05 09:20:19 +0300

    CategorieFilterScreen : fix le background

--- lib/feature/categorieFilter/categorie_filter_screen.dart
+++ lib/feature/categorieFilter/categorie_filter_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 
 class CategorieFilterScreen extends StatelessWidget {
   const CategorieFilterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(children: const [
+        BlurHash(
+          hash: "LaQvwRay_3WB~qt7M{ofD%RjRjay",
+          imageFit: BoxFit.cover,
+        ),
+      ]),
+    );
   }
 }
