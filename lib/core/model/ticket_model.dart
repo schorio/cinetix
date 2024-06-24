8bac66291eaa763ff2549a357a618a994cbb1855
schorio <schorioignace@gmail.com>
2024-06-24 11:21:01 +0300

    TicketModel : fix les types du validation des tickets

--- lib/core/model/ticket_model.dart
+++ lib/core/model/ticket_model.dart
@@ -3,12 +3,12 @@ import 'package:cinetix/core/model/film_model.dart';
 class Ticket {
   final Film film;
   final List<String> places;
-  final String type;
+  final bool valider;
 
   Ticket({
     required this.film,
     required this.places,
-    required this.type,
+    required this.valider,
   });
 }
 
@@ -16,36 +16,36 @@ List<Ticket> lesTickets = [
   Ticket(
     film: prochainement[0],
     places: ["A5", "B8", "A2"],
-    type: "Valider",
+    valider: true,
   ),
   Ticket(
     film: prochainement[3],
     places: ["A5", "A5"],
-    type: "En examination",
+    valider: false,
   ),
   Ticket(
     film: prochainement[1],
     places: ["A3", "B2"],
-    type: "Valider",
+    valider: true,
   ),
   Ticket(
     film: prochainement[0],
     places: ["B7", "A8"],
-    type: "Valider",
+    valider: true,
   ),
   Ticket(
     film: prochainement[2],
     places: ["A5", "B8", "A2"],
-    type: "En examination",
+    valider: false,
   ),
   Ticket(
     film: prochainement[1],
     places: ["A5", "B6", "A1"],
-    type: "En examination",
+    valider: false,
   ),
   Ticket(
     film: prochainement[3],
     places: ["A5", "A9"],
-    type: "Valider",
+    valider: true,
   ),
 ];
